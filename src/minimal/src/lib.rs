3c8c1ca7c06d4b69886a6600433c3f2cbd10146d
Kasper Nielsen <kasper0406@gmail.com>
2021-04-04 21:18:41 +0200

    Make the browser say things

--- src/minimal/src/lib.rs
+++ src/minimal/src/lib.rs
@@ -1,11 +1,59 @@
 extern crate wasm_bindgen;
 // extern crate console_error_panic_hook;
 // extern crate web_sys as ws;
+// extern crate js_sys;
 
 use wasm_bindgen::prelude::*;
 // use console_error_panic_hook::set_once as set_panic_hook;
+// use js_sys::SharedArrayBuffer;
+// use js_sys::Float32Array;
 
 #[wasm_bindgen]
-pub fn synth_callback() {
-    // ws::console::log_1(&"Inside synth callback in Rust!".into());
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn test_rust() -> f64 {
+    log("Inside rust 😁");
+    123.8
+}
+
+#[wasm_bindgen]
+pub struct Synth {
+    sample_rate: f64,
+    phase: f64,
+    samples: Vec<f32>,
+}
+
+#[wasm_bindgen]
+impl Synth {
+    pub fn new(sample_rate: f64) -> Synth {
+        log("Initializing 🎹");
+
+        let sample_size = 1024;
+        let mut samples = Vec::with_capacity(sample_size);
+        for i in 0 .. sample_size {
+            let input = ((i as f64) / (sample_size as f64)) * 2f64 * std::f64::consts::PI;
+            let value = input.sin() as f32;
+            samples.push(value);
+        }
+
+        Synth {
+            sample_rate,
+            phase: 0.0,
+            samples
+        }
+    }
+
+    pub fn generate(&mut self, output: &mut [f32]) {
+        let frequency = 261.62;
+
+        for i in 0 .. output.len() {
+            let idx = (self.phase * output.len() as f64) as usize;
+            output[i] = self.samples[idx] * 0.2;
+            self.phase = (self.phase + frequency / self.sample_rate).fract();
+        }
+    }
 }
--- src/ui/main.js
+++ src/ui/main.js
@@ -1,5 +1,5 @@
-// const rust = import("./pkg");
-const minimalLoader = import("../minimal/pkg/minimal")
+// const wasm = import("./pkg");
+// const minimalLoader = import("../minimal/pkg/minimal")
 
 console.log("Defining functions...");
 
@@ -10,29 +10,109 @@ export function setup_audio() {
         console.log('Inside audio setup code');
 
         class SynthProcessor extends AudioWorkletProcessor {
+            #wasmImportObj = (() => {
+                let wasm;
+
+                function setWasm(new_wasm) {
+                    wasm = new_wasm;
+                }
+
+                class Synth {
+                    static __wrap(ptr) {
+                        const obj = Object.create(Synth.prototype);
+                        obj.ptr = ptr;
+                        return obj;
+                    }
+
+                    free() {
+                        wasm.__wbindgen_free(this.buffer_ptr, this.buffer_len * 4);
+
+                        const ptr = this.ptr;
+                        this.ptr = 0;
+                        wasm.__wbg_synth_free(ptr);
+                    }
+
+                    constructor(sample_rate) {
+                        var ret = wasm.synth_new(sample_rate);
+                        const instance = Synth.__wrap(ret);
+
+                        instance.buffer_len = 128;
+                        instance.buffer_ptr = wasm.__wbindgen_malloc(instance.buffer_len * 4);
+                        instance.buffer = new Float32Array(wasm.memory.buffer)
+                                            .subarray(instance.buffer_ptr / 4, instance.buffer_ptr / 4 + instance.buffer_len);
+
+                        return instance;
+                    }
+
+                    generate(output) {
+                        wasm.synth_generate(this.ptr, this.buffer_ptr, this.buffer_len);
+                        output.set(this.buffer);
+                    }
+                }
+
+                let cachedMemory = null;
+                function getMemory() {
+                    if (cachedMemory == null || cachedMemory.buffer !== wasm.memory.buffer) {
+                        cachedMemory = new Uint8Array(wasm.memory.buffer);
+                    }
+                    return cachedMemory;
+                }
+
+                function decodeUtf8(bytes) {
+                    return decodeURIComponent(bytes.reduce((p, c) => {
+                        if (typeof p === "string") {
+                            return p + "%" + c.toString(16)
+                        } else {
+                            return "%" + p.toString(16) + "%" + c.toString(16);
+                        }
+                    }));
+                }
+
+                function getStringFromWasm(ptr, len) {
+                    const bytes = getMemory().subarray(ptr, ptr + len);
+                    const utf8decoded = decodeUtf8(bytes);
+                    return utf8decoded;
+                }
+
+                const imports = {};
+                imports.wbg = {};
+                imports.wbg.__wbg_log_9686041a8580e419 = function(arg0, arg1) {
+                    console.log(getStringFromWasm(arg0, arg1));
+                };
+                imports.wbg.__wbindgen_throw = function(arg0, arg1) {
+                    throw new Error(getStringFromWasm(arg0, arg1));
+                };
+
+                imports.control = {};
+                imports.control.setWasm = setWasm;
+
+                imports.Synth = Synth;
+
+                return imports;
+            })();
+
             constructor(...args) {
                 super(...args);
                 console.log("Inside SynthProcessor...");
 
                 this.port.onmessage = (e) => {
-                    let wasmBytes = e.data;
-                    console.log("Received WASM bytes", wasmBytes);
+                    const wasmBytes = e.data[0];
+                    const sampleRate = e.data[1];
 
-                    var importObject = {
-                        imports: {
+                    WebAssembly.instantiate(wasmBytes, this.#wasmImportObj).then(wasm => {
+                        console.log("Loaded wasm wasm xD");
+                        this.wasm = wasm.instance.exports;
+                        this.#wasmImportObj.control.setWasm(this.wasm);
 
-                        }
-                    }
-                    WebAssembly.instantiate(wasmBytes, importObject).then(rust => {
-                        console.log("Loaded rust wasm xD");
-                        this.rust = rust.instance.exports;
+                        this.wasm.test_rust();
+                        this.synth = new this.#wasmImportObj.Synth(sampleRate);
                     });
                 };
             }
 
             process (inputs, outputs, parameters) {
-                if (this.rust !== undefined) {
-                    this.rust.synth_callback(outputs);
+                if (this.synth !== undefined) {
+                    this.synth.generate(outputs[0][0]);
                 }
 
                 return true;
@@ -42,22 +122,22 @@ export function setup_audio() {
         registerProcessor("synth-processor", SynthProcessor);
     `;
 
-    // minimal.init();
-
     var blob = new Blob([audioCode], { type: 'text/javascript' });
     var workerUrl = URL.createObjectURL(blob);
 
     console.log("Filename", __filename);
-    let wasmLoader = fetch("da8817554716fd3bb361.module.wasm");
+    let wasmLoader = fetch("minimal.wasm");
+    let wasmImportLoader = fetch("minimal.js");
 
     const audioContext = new AudioContext();
     audioContext.audioWorklet.addModule(workerUrl).then(_ => {
         const synthNode = new AudioWorkletNode(audioContext, "synth-processor");
         synthNode.connect(audioContext.destination);
 
-        wasmLoader.then(response => response.arrayBuffer())
-            .then(bytes => {
-                synthNode.port.postMessage(bytes);
-            })
+        Promise.all([ wasmLoader, wasmImportLoader ])
+            .then((responses) => Promise.all([ responses[0].arrayBuffer(), responses[1].text() ]))
+            .then((wasm) => {
+                synthNode.port.postMessage([ wasm[0], audioContext.sampleRate ]);
+            });
     });
 };
